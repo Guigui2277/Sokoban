830d00789f4470544256b96726f5bafd165f5563
guigui2277 <guillaume.antoines@etudiant.univ-rennes.fr>
2025-12-09 21:17:42 +0100

    changements sur l'affichage

--- sokoban.c
+++ sokoban.c
@@ -21,8 +21,6 @@
 // Définition de la taille du tableau.
 #define MAXLIG 12
 #define MAXDEP 10000
-#define MAXECH 3
-#define MINECH 1
 #define TAILLE_FICHIER 50
 
 typedef char t_plateau[MAXLIG][MAXLIG];
@@ -33,6 +31,7 @@ typedef struct{
 	int posx; // position horizontale du joueur
 	int posy; // position verticale du joueur
 	int nbDep; // nombre de déplacements effectués
+	int animation; // nombre de jeu->animations sur l'entête
 	t_plateau plateau; // déclaration du plateau de jeu
 	typeDeplacements historiqueDep; // déclaration du tableau des déplacements
 } t_partie;
@@ -47,22 +46,12 @@ const char JOUEUR_CIBLE = '+';
 const char CAISSE_CIBLE = '*';
 const char CASE = ' ';
 
-// Définition des touches de déplacement.
-const char HAUT = 'z';
-const char BAS = 's';
-const char GAUCHE = 'q';
-const char DROITE = 'd';
-const char QUITTER = 'x';
-const char RECOMMENCER = 'r';
-const char RETOUR = 'u';
-const char ZOOMER = '+';
-const char DEZOOMER = '-';
-
 // Définition des caractères de déplacement
 const char DEP_GAUCHE = 'g';
 const char DEP_DROITE = 'd';
 const char DEP_HAUT = 'h';
 const char DEP_BAS = 'b';
+const char RETOUR = 'u';
 const char CAISSE_GAUCHE = 'G';
 const char CAISSE_DROITE = 'D';
 const char CAISSE_HAUT = 'H';
@@ -72,7 +61,6 @@ const char CAISSE_BAS = 'B';
 // liste des procédures déclarées
 void chargerPartie(t_plateau plateau, char fichier[]);
 void chargerDeplacements(typeDeplacements t, char fichier[], int * nb);
-int max_tab(typeDeplacements t);
 void afficher_entete(t_partie *jeu, char fichier[], char deplacements[]);
 void afficher_plateau(t_partie *jeu);
 void chercher_joueur(t_partie *jeu);
@@ -93,6 +81,8 @@ int main(){
 	t_partie jeu;
 	jeu.posx = 0; // initialisation de la position
 	jeu.posy = 0; 
+	jeu.nbDep = 0; // initialisation du nombre de déplacements
+	jeu.animation = 1;
 	int maxTaille; // nombre de caractères dans le tableau des déplacements
 	char fichier[TAILLE_FICHIER]; // le nom du fichier de la partie
 	char deplacements[TAILLE_FICHIER]; // le nom du fichier des déplacements
@@ -111,16 +101,19 @@ int main(){
 	afficher_plateau(&jeu);
 	chercher_joueur(&jeu);
 	// tant qu'il y a des caisses à déplacer
-	for (jeu.nbDep = 0; jeu.nbDep < maxTaille; jeu.nbDep++) {
-		usleep(500000); // pause de 0.5 seconde
-	 	Analyse(&jeu, fichier, deplacements);
+	while (jeu.nbDep < maxTaille && !gagner(&jeu)) {
+		usleep(250000); // pause de 0.25 seconde
+		Analyse(&jeu, fichier, deplacements);
+		jeu.nbDep++;
 	}
 
 	// affichage des résultats
 	if (gagner(&jeu)) {
 		printf("La suite de déplacements %s est bien une solution pour la partie %s.\n", fichier, deplacements);
-		printf("Elle contient %d déplacements.\n", jeu.nbDep);
-
+		if (jeu.nbDep < maxTaille){
+			printf("Elle contenait de base %d caractères\n", maxTaille);
+		}
+		printf("La partie contient actuellement %d déplacements.\n", jeu.nbDep);
 	} 
 	
 	else {
@@ -161,7 +154,7 @@ void chargerPartie(t_plateau plateau, char fichier[]){
 * @param t type : tableau, entrée/sortie, importe le tableau des déplacements
 * @param fichier type : chaine, entrée, fichier des déplacements 
 * @param nb type : entier, entrée/sortie, nombre de caractères chargés
-* @return résultat : chargement de la partie
+* @return résultat : chargement du fichier des caractères
 */
 
 void chargerDeplacements(typeDeplacements t, char fichier[], int * nb){
@@ -185,14 +178,6 @@ void chargerDeplacements(typeDeplacements t, char fichier[], int * nb){
         }
     }
     fclose(f);
-}
-
-int max_tab(typeDeplacements t){
-	int max = 0;
-	while (t[max] == '\0'){
-		max++;
-	}
-	return max;
 }
 
 /**
@@ -223,17 +208,15 @@ void enregistrerPartie(t_plateau plateau, char fichier[]){
 * @param plateau type : tableau, entrée/sortie, importe le tableau de jeu
 * @param fichier type : entier, entrée, fichier de la partie chargée
 * @param deplacements type : entier, entrée, fichier des déplacements
-* @return résultat : affichage du plateau
+* @param animation : entier, entrée, affiche une animation de 1 à 3 points
+* @return résultat : affichage de l'entête du plateau
 */
 
 void afficher_entete(t_partie *jeu, char fichier[], char deplacements[]){
 
-	// 'static' permet à la variable de garder sa valeur entre deux appels de la fonction
-    static int point = 1; 
-
     // Gestion du cycle
-    if (point > 3) {
-        point = 1;
+    if (jeu->animation > 3) {
+        jeu->animation = 1;
     }
 
 	system("clear");
@@ -241,22 +224,22 @@ void afficher_entete(t_partie *jeu, char fichier[], char deplacements[]){
 	printf(" Nom du fichier de déplacements : %s\n\n", deplacements);
 	printf(" Nombre de déplacement : %d\n\n", jeu->nbDep);
 
-	if (point == 1){
+	if (jeu->animation == 1){
 		printf(" Analyse en cours.\n\n");
 	} 
-	else if (point == 2){
+	else if (jeu->animation == 2){
 		printf(" Analyse en cours..\n\n");
 	}
-	else if (point == 3){
+	else if (jeu->animation == 3){
 		printf(" Analyse en cours...\n\n");
 	}
-	point++;
+	jeu->animation++;
 }
 
 /**
 * @brief affiche le plateau de jeu
 * @param plateau type : tableau, entrée/sortie, importe le tableau de jeu
-* @return résultat : affiche du nombre de caractères selon l'echelle sur plateau
+* @return résultat : affiche du nombre de caractères sur le tableau
 */
 
 void afficher_plateau(t_partie *jeu) {
@@ -313,7 +296,7 @@ void chercher_joueur(t_partie *jeu){
 	ou position de la caisse
 * @param nbDep type : entier, entrée/sortie, nombre de déplacements effectués
 * @param last type : caractère, entrée, dernier caractère de déplacement
-* @return résultat : la caisse peut/peut pas se déplacer
+* @return résultat : déplacement ou non dans le plateau
 */
 
 void conditions_dep(t_partie *jeu, int depx, int depy, char last){
@@ -406,6 +389,52 @@ void deplacer_caisse(t_partie *jeu, int depx, int depy, int casx, int casy){
 	}
 }
 
+void annuler_deplacer(t_partie *jeu, char last){
+
+	int depx = jeu->posx; //case de déplacement horizontale
+	int depy = jeu->posy; //case de déplacement verticale
+	int casx; // case de déplacement de la caisse
+	int casy; 
+	int ancienx; // ancienne case de la caisse
+	int ancieny; 
+
+	if (last == DEP_HAUT || last == CAISSE_HAUT) {
+		depx++; //déplacement vers le Haut
+	}
+	else if (last == DEP_BAS || last == CAISSE_BAS) {
+		depx--; //déplacement vers le Bas
+	}
+	else if (last == DEP_GAUCHE || last == CAISSE_GAUCHE) {
+		depy++; //déplacement à Droite
+	}
+	else if (last == DEP_DROITE || last == CAISSE_DROITE) {
+		depy--; //déplacement à Gauche
+	}
+	// la case de destination de la caisse correspond a l'ancienne du personnage
+	casx = jeu->posx;
+	casy = jeu->posy;
+	// changement de position du caractère
+	deplacer_joueur(jeu, depx, depy);
+	// si les anciens déplacements correspondent à celui d'une caisse
+	if (last == CAISSE_HAUT ||
+		last == CAISSE_BAS ||
+		last == CAISSE_GAUCHE ||
+		last == CAISSE_DROITE) {
+		// ancienne position de la caisse
+		ancienx = casx + (casx - jeu->posx);
+		ancieny = casy + (casy - jeu->posy);
+		// si ancienne position de la caisse = cible
+		if (jeu->plateau[ancienx][ancieny] == CAISSE_CIBLE) {
+			jeu->plateau[ancienx][ancieny] = CIBLE;
+		}
+		else {
+			jeu->plateau[ancienx][ancieny] = CASE;
+		}
+		//déplacement de la caisse
+		deplacer_caisse(jeu, ancienx, ancieny, casx, casy);
+	}
+}
+
 /**
 * @brief cette procédure contient les touches et conditions pour Analyse.
 * @param plateau type : tableau, entrée/sortie, importe le tableau de jeu
@@ -414,7 +443,6 @@ void deplacer_caisse(t_partie *jeu, int depx, int depy, int casx, int casy){
 * @param posx type : entier, entrée/sortie, position verticale du joueur
 * @param posy type : entier, entrée/sortie, position horizontale du joueur
 * @param nbDep type : entier, entrée/sortie, nombre de déplacements
-* @param echelle type : entier, entrée, taille du tableau
 * @param fichier type : chaine, entrée, fichier de sauvegarde
 * @return résultat : permet de Analyse au jeu
 */
@@ -451,7 +479,8 @@ void Analyse(t_partie *jeu, char fichier[], char deplacements[]){
 			last = toupper(last); // conversion en majuscule
 		}
 		// si les touches sont celles de déplacements on utilise les conditions
-		conditions_dep(jeu, depx, depy, last);
+		
+			conditions_dep(jeu, depx, depy, last);
 		
 		system("clear");
 		afficher_entete(jeu, fichier, deplacements);
